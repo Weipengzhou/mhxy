c4d2ac405b212668cd3a39f6d596e7009ea8509c
weipengzhou <492020314@qq.com>
2019-10-17 22:36:19 +0800

    [fix]修改middleware函数写法

--- wechat/config.js
+++ wechat/config.js
@@ -2,6 +2,6 @@ module.exports = {
     wechat: {
         appID: 'wxfc36d4b6da365492',
         appSecret: 'b558341f3302ad43b1fd5c83a6d5504b',
-        token: 'hellword'
+        token: 'helloword'
     }
 };
--- wechat/index_middleware.js
+++ wechat/index_middleware.js
@@ -1,17 +1,19 @@
 var sha1 = require('sha1');
 
 exports.get = function(opts) {
-    return function *(next) {
+    return async (ctx, next) => {
         var token = opts.token;
-        var signature = this.query.signature;
-        var nonce = this.query.nonce;
-        var timestamp = this.query.timestamp;
-        var echostr = this.query.echostr;
+        var signature = ctx.query.signature;
+        var nonce = ctx.query.nonce;
+        var timestamp = ctx.query.timestamp;
+        var echostr = ctx.query.echostr;
         var str = [token, timestamp, nonce].sort().join('');
         var sha = sha1(str);
-
+      
         if (sha === signature) {
-            this.body = echostr + '';
+          ctx.body = echostr + '';
+        } else {
+          ctx.body = '失败';
         }
-    };
+      };
 };
